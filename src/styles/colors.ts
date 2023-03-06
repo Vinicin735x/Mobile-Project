462ef030968e1bee0db39c308f2e8471dbffaf49
Vinicin735x <vini08.mantovani@gmail.com>
2023-03-06 12:22:16 -0300

    esse otario

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#774891',
+    secondary: '#090486',
+    thrid: '#8784B6',
+    thirdLight: 'rgba(217,217,217, 0.8)',
+    black: '#000',
+    white: '#FFF'
+}
