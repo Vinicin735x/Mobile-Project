56c57ad25002ee15ce2f4eadbd192e47c51033a1
Vincin735x <vini08.mantovani@gmail.com>
2023-03-17 22:34:31 -0300

    sepa acabei

---
+++ src/screens/Slider1/index.tsx
@@ -0,0 +1,33 @@
+import { FlatlList, ImageBackground, View} from 'react-native';
+import {IPage} from '../../../App';
+import {
+    ComponentButtonSlider, ComponentListMarker, ComponentTitleSlider
+} from '../../components';
+import {styles} from './styles';
+export function Slider1({setPageI}: IPage) {
+   const slide1 = require("../../assets/slide1.png")
+   const slide1Texts = [
+    { id: '1', text: 'Localize seu pedido.'},
+    { id: '2', text: 'Acompanhe todo o percurso.'},
+   ] 
+   return (
+    <ImageBackground source={slide1} style={styles.container} >
+        <View style={styles.panel}>
+            <ComponentTitleSlider titleI='Sistema de rastreamento' />
+            <FlatlList
+            data={slide1Texts}
+            renderItem={({item}) =>
+                <ComponentListMarker key={item.id} textMarker={item.text} />
+            }
+            keyExtractor={(item) => item.id}
+            />
+        </View>
+        <View style={styles.buttonSlider}>
+            <ComponentButtonSlider onPressI={() => setPageI(1)} />
+            <ComponentButtonSlider onPressI={() => setPageI(2)} />
+            <ComponentButtonSlider onPressI={() => setPageI(3)} />
+            <ComponentButtonSlider onPressI={() => setPageI(4)} />
+        </View>
+    </ImageBackground>
+   );
+}
