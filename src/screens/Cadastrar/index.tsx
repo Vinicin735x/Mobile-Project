d17ba0843f0117a252d57471c2e60f57b0b5d630
Vinicin735x <vini08.mantovani@gmail.com>
2023-08-28 12:29:24 -0300

    Foguetin

--- src/screens/Cadastrar/index.tsx
+++ src/screens/Cadastrar/index.tsx
@@ -1,14 +1,67 @@
-import React from 'react';
-import { View, Text, KeyboardAvoidingView, TextInput} from "react-native"
+import React, { useEffect, useState } from 'react';
+import { View, Text, KeyboardAvoidingView, TextInput, Alert} from "react-native"
 import { styles } from "./styles"
 import { MaterialIcons, Fontisto, FontAwesome, AntDesign, FontAwesome5  } from '@expo/vector-icons';
 import { colors } from '../../styles/colors';
-import { ComponentButtonInterface } from '../../components';
+import { ComponentButtonInterface, ComponentLoading } from '../../components';
 import { LoginTypes } from '../../navigations/login.navigation';
+import { IRegister } from "../../services/data/User"
+import { apiUser } from '../../services/data';
+import { AxiosError } from 'axios';
+
+export interface IErrorApi{
+    errors: {
+        rule: string
+        field: string
+        message: string
+    }[]
+}
 
 export function Cadastrar({navigation}: LoginTypes) {
+    const[data, setData] = useState<IRegister>()
+    const [isLoading, setIsloading] = useState(true)
+    async function handleRegister(){
+        try{
+            setIsloading(true)
+            if(data?.name && data.email && data.password){
+                const response = await apiUser.register(data)
+                Alert.alert('$response.data.name) sucesso chefia!')
+                navigation.navigate('Login')
+            } else {
+                Alert.alert("Preencha todos os campos ae meu rei!")
+            }
+
+        } catch (error) {
+            const err = error as AxiosError 
+            const errData = err.response?.data as IErrorApi
+            let message = ""
+            if (errData) {
+                for (const iterator of errData.errors)
+                message = '${message} ${NodeIterator.message} \n'
+            }
+
+        } finally {
+            setIsloading(false)
+        }      
+    }
+
+    function handleChange(item: IRegister) {
+        setData({ ...data, ...item})
+    }
+    useEffect(()=> {
+        setTimeout(()=> {
+            setIsloading(false)
+        }, 5500);
+    }, [])
+    
     return (
-        <View style={styles.container}>
+        <>
+        {isLoading ? (
+            <ComponentLoading />
+
+        ):(
+
+            <View style={styles.container}>
             <KeyboardAvoidingView>
                 <Text style={styles.title}>Cadastro</Text>
                 <View style={styles.formRow}>
@@ -19,6 +72,7 @@ export function Cadastrar({navigation}: LoginTypes) {
                     keyboardType="email-address"
                     autoCapitalize="none"
                     style={styles.input}
+                    onChangeText={(i) => handleChange({name: i})}
                 />    
                 </View>
                 <View style={styles.formRow}>
@@ -29,6 +83,7 @@ export function Cadastrar({navigation}: LoginTypes) {
                     keyboardType="email-address"
                     autoCapitalize="none"
                     style={styles.input}
+                    onChangeText={(i) => handleChange({email: i})}
                 />    
                 </View>
                 <View style={styles.formRow}>
@@ -39,14 +94,20 @@ export function Cadastrar({navigation}: LoginTypes) {
                     secureTextEntry={true}
                     autoCapitalize="none"
                     style={styles.input}
+                    onChangeText={(i) => handleChange({password: i})}
                 />    
                  </View>
-            <ComponentButtonInterface title='Salvar' type='primary' onPressI={()=>{navigation.navigate('Drawer')}} />
+            <ComponentButtonInterface title='Salvar' type='primary' onPressI={handleRegister} />
             <ComponentButtonInterface title='Voltar' type='secondary' onPressI={()=> navigation.navigate ('Login')} />
         
             </KeyboardAvoidingView>
         </View>
-
+    
             
+    
+        )}
+        </>
     )
 }
+
+
---
+++ src/services/data/User/index.ts
@@ -0,0 +1,33 @@
+import {api} from"../../api"
+
+// ? significa que os dados abaixo são opcionais
+
+export interface IRegister {
+    name?: string
+    email?: string
+    password?: string
+}
+export interface IAutenticate {
+    email?: string
+    password?: string
+}
+export interface IUser {
+    id: number
+    name: string
+    email: string
+}
+export interface IUserLogin {
+    user: string
+   token: {
+    expires_at:string
+   }
+}
+class UserData {
+    register(data: IRegister) {
+        return api.post('/register', data)
+    }
+    login(data:IAutenticate) {
+        return api.post<IUserLogin>('/login', data)
+    }
+}
+export default new UserData()
